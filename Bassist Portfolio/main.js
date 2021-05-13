2146eb559ccc2e8734213e5437a5ca5b8293ea8c
Punishedvic <64317029+Punishedvic@users.noreply.github.com>
2021-05-13 13:54:51 -0300

    Added bg images and AOS config

--- Bassist Portfolio/main.js
+++ Bassist Portfolio/main.js
@@ -1,3 +1,12 @@
+var options = {
+    width: 680,
+    height: 320,
+    video: "633607105",
+    autoplay: false
+}
+
+new Twitch.Player("twitch-embed", options);
+
 AOS.init({
     duration: 1200,
 })
