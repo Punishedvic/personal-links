f459f60f853cf7cd77188212d6783658c85bb3ad
Punishedvic <64317029+Punishedvic@users.noreply.github.com>
2021-05-17 01:10:44 -0300

    Mobile First enhancements

--- Bassist Portfolio/main.js
+++ Bassist Portfolio/main.js
@@ -1,12 +1,3 @@
-var options = {
-    width: 680,
-    height: 320,
-    video: "633607105",
-    autoplay: false
-}
-
-new Twitch.Player("twitch-embed", options);
-
 AOS.init({
     duration: 1200,
 })
