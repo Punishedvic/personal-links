949f5c7a8cd94606523c05d55ad26e17ce8e6e49
Punishedvic <64317029+Punishedvic@users.noreply.github.com>
2021-05-24 02:27:21 -0300

    Added link divs - animation and description

--- Bassist Portfolio/main.js
+++ Bassist Portfolio/main.js
@@ -1,3 +1,23 @@
+// FUNCION Animations
+
 AOS.init({
     duration: 1200,
+})
+
+
+// DESPLEGABLES 
+
+
+const boton = document.querySelectorAll('button');
+for (e of boton) {
+    e.classList.add('faq-toggle');
+}
+
+const toggle = document.querySelectorAll('.faq-toggle');
+
+
+toggle.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
+    })
 })
